480e55417c40d9292c181d3c55e72c79f7a784b7
umeshkumar <ukm3423@gmail.com>
2024-07-10 16:21:10 +0530

    try

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Kaise Ho Guru !</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
+       
       </header>
     </div>
   );
